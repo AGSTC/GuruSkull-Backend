44ca95f9acec9ee1129d190c470859997ce02ff6
YourUsername <your.email@example.com>
2025-10-01 12:05:49 +0530

    made push from database

--- Common_Lib/GuruSkull/guruskull/admin.py
+++ Common_Lib/GuruSkull/guruskull/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *  # imports all models from models.py
 
-# Register your models here.
+# Dynamically register all models in the app
+from django.apps import apps
+
+app = apps.get_app_config('guruskull')
+for model_name, model in app.models.items():
+    admin.site.register(model)
